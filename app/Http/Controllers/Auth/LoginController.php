a757ffdfcb397d83199cda56d7e3f136b0c1876a
Richie-Z <zakariarichie@yahoo.com>
2024-11-18 23:06:56 +0700

    feat: introduce login

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'username' => 'required|exists:users,username',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($request->only(['username', 'password']), $request->boolean('remember'))) {
+            return redirect()->route('admin.dashboard');
+        }
+
+        return back()->withErrors(['error' => 'Password tidak cocok']);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::prefix('login')
+    ->name('login')
+    ->middleware('guest')
+    ->group(function () {
+        Route::get('', 'Auth\LoginController@showLoginForm');
+        Route::post('', 'Auth\LoginController@login')->name('.post');
+    });
+
+Route::prefix('admin')
+    ->name('admin.')
+    ->middleware('auth')
+    ->group(function () {
+        Route::get('/', 'Admin\DashboardController@index')->name('dashboard');
+    });
