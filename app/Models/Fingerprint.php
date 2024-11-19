aa94c510d0245b2557b254a26f18cfc1807c6fbf
Richie-Z <zakariarichie@yahoo.com>
2024-11-19 21:27:25 +0700

    feat: introduce fingerprint

---
+++ app/Models/Fingerprint.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ *
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $ip
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Fingerprint newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Fingerprint newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Fingerprint query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Fingerprint whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Fingerprint whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Fingerprint whereIp($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Fingerprint whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Fingerprint whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class Fingerprint extends Model
+{
+    protected $fillable = [
+        'name',
+        'ip'
+    ];
+}
